66bffdedefe0f81e6edec351920e6059905af4a5
Godtokoo <79078025+Godtokoo666@users.noreply.github.com>
2024-06-19 02:21:51 +0800

    Delete test directory

--- test/utils.py
+++
@@ -1,9 +0,0 @@
-from ..app.models import User, Group, privileges, db
-
-
-group=Group()
-group.name='Default'
-group.description='Default group'
-group.priv=privileges['PRIV_USER']
-db.session.add(group)
-db.session.commit()
